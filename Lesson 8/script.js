4182247b0e856b8767c8860fea27035deaee82df
sp14bw <54948351+sp14bw@users.noreply.github.com>
2019-11-08 15:50:06 -0500

    Lab 8

---
+++ Lesson 8/script.js
@@ -0,0 +1,11 @@
+document.getElementById('one').style.color="blue";
+document.getElementById("one").style.fontSize = "xx-large";
+
+document.getElementById("two").style.color= "green";
+document.getElementById("two").style.fontSize = "xx-small";
+document.getElementById("two").style.margineleft = "150px";
+
+document.getElementById("three").style.color = "orange";
+
+
+document.getElementsByTagName("textMagic") = "textMagic";
