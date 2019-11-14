799feaaa165b0db784053e6f3952ea8ea1d0da89
sp14bw <54948351+sp14bw@users.noreply.github.com>
2019-11-13 19:44:16 -0500

    LAB NINE

---
+++ Lesson 9/Lab9Exercise 3.js
@@ -0,0 +1,13 @@
+function addition (){
+
+var a = document.getElementById("num1").value;
+var b = document.getElementById("num2").value;
+var output = parseFloat (a) + parseFloat (b);
+if (!Number.isInteger(output)) {
+  output = "OUTPUT";
+} else {
+  output = parseFloat(a) + parseFloat(b);
+};
+document.getElementById("out").innerHTML = output;
+
+};
